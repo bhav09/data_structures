476da7cb4a1f9a6c46958540570d58415e2c5e7d
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2021-02-11 10:15:56 +0530

    Add files via upload

---
+++ Arrays/NegativeLeftPositiveRight.py
@@ -0,0 +1,22 @@
+#Move all negative numbers to beginning and positive to end with constant extra space
+
+def CustomShuffle(arr):
+	neg=[]
+	for i in arr:
+		#print(i)
+		if (i<0):
+			val=i
+			neg.append(val)
+	#print(neg)
+	for value in arr:
+		if value<0:
+			arr.pop(arr.index(value))
+	neg=neg+arr
+	return neg
+
+arr=[]
+for i in range(5):
+	arr.append(int(input('Enter Element:')))
+new_arr=CustomShuffle(arr)
+
+print('After Custom Shuffling:',new_arr)
